6d0c5f2a332797e29c7db945b65e7fbd27903808
Ann-Milovanova <annamilovanova30@gmail.com>
2024-09-25 23:02:46 +0300

    Update Lev3(1).py

--- Lev3(1).py
+++ Lev3(1).py
@@ -1,22 +1,27 @@
 import math
 a=0.1
 b=1
-i=0
-sum=0
 h=0.1
 e=0.0001
-s=999
 y=0
 for x in range(1,11):
+    i = 0
     x=x/10
-    while abs(s)>e:
-        y=math.cos(x)
-        s=(((-1)**i)*x**(2*i))/(math.factorial(2*i))
-        sum=sum+s
-        i = i + 1
-    else:
-        sum = sum - s
+    s = 999
+    sum = 0
+    y = math.cos(x)
+    f = True
+    while f:
+        if abs(s)>e:
+            s=(((-1)**i)*x**(2*i))/(math.factorial(2*i))
+            sum=sum+s
+            i = i + 1
+            f=True
+        else:
+            sum = sum - s
+            f=False
     print(sum,y)
+
 
 
 
