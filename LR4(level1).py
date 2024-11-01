7928a7939538b7b1f3f2320ccc68f4705176909f
Ann-Milovanova <annamilovanova30@gmail.com>
2024-11-01 16:02:44 +0300

    Update LR4(level1).py

--- LR4(level1).py
+++ LR4(level1).py
@@ -1,7 +1,3 @@
-from random import randint
-print(randint(1, 33))
-print(randint(1, 9))
-print(randint(1, 14))
 
 #ур1№33
 a=[[1, 3, 5, -7, -8], [2, 3, -9, -8, 3]]
@@ -11,4 +7,15 @@ for i in a:
         if j>0:
             b.append(j)
 print(b)
-
+#ур2№
+#ур3№2
+m=[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+l=0
+for i in m:
+    l=l+1
+for i in range(len(m)):
+    m[0][i]=0
+    m[len(m)-1][i]=0
+    m[i][0]=0
+    m[i][len(m)-1]=0
+print(m)
