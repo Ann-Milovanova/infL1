bf296f18448898a2f7ab62e9bb8230c7e1d40372
Ann-Milovanova <annamilovanova30@gmail.com>
2024-12-24 12:25:17 +0300

    Update LR6(7)(level1).py

--- LR6(7)(level1).py
+++ LR6(7)(level1).py
@@ -18,11 +18,9 @@ class Competition:
         self.participants.append(participant)
 
     def sort_participants(self):
-        # Сортируем участников по сумме попыток в порядке убывания
         for i in range(len(self.participants)):
             for j in range(0, len(self.participants) - i - 1):
                 if self.participants[j].total_score() < self.participants[j + 1].total_score():
-                    # Меняем местами
                     self.participants[j], self.participants[j + 1] = self.participants[j + 1], self.participants[j]
 
     def display_results(self):
@@ -39,62 +37,58 @@ competition.add_participant("Билявский", "Спорт", 8.0, 8.5)
 competition.display_results()
 
 #ур2№5 Соревнования по прыжкам на лыжах со 120-метрового трамплина оценивают 5 судей. Каждый судья выставляет оценку за стиль прыжка по 20-балльной шкале. Меньшая и большая оценки отбрасываются, остальные суммируются. К этой сумме прибавляются очки за дальность прыжка: 120 м – 60 очков, за каждый метр превышения добавляются по 2 очка, при меньшей дальности отнимаются 2 очка за каждый метр. Получить итоговую таблицу соревнований, содержащую фамилию и итоговый результат для каждого участника в порядке занятых мест.
-class Participant:
-    def __init__(self, name, scores, jump_distance):
-        self.name = name
-        self.scores = scores
-        self.jump_distance = jump_distance
-        self.final_score = self.calculate_final_score()
+
+  Participant:
+    def __init__(self, surname, scores, distance):
+        self.surname = surname
+        self.scores = scores  # Список оценок судей
+        self.distance = distance
 
     def calculate_final_score(self):
-        
-        min_score = max_score = self.scores[0]
+        # Отбрасываем меньшую и большую оценки
+        sorted_scores = self.scores[:]
+        # Сортировка для нахождения меньшей и большей оценок
+        for i in range(len(sorted_scores)):
+            for j in range(0, len(sorted_scores) - i - 1):
+                if sorted_scores[j] > sorted_scores[j + 1]:
+                    sorted_scores[j], sorted_scores[j + 1] = sorted_scores[j + 1], sorted_scores[j]
 
-        for score in self.scores:
-            if score < min_score:
-                min_score = score
-            if score > max_score:
-                max_score = score
+        trimmed_scores = sorted_scores[1:-1]
+        score_sum = sum(trimmed_scores)
 
-        total_style_score = 0
-        for score in self.scores:
-            if score != min_score and score != max_score:
-                total_style_score += score
-                
-        if self.jump_distance > 120:
-            total_style_score += 60 + (self.jump_distance - 120) * 2
+        if self.distance >= 120:
+            distance_score = 60 + (self.distance - 120) * 2
         else:
-            total_style_score += 60 - (120 - self.jump_distance) * 2
+            distance_score = 60 - (120 - self.distance) * 2
+        return score_sum + distance_score
 
-        return total_style_score
+class Competition:
+    def __init__(self):
+        self.participants = []
 
+    def add_participant(self, surname, scores, distance):
+        participant = Participant(surname, scores, distance)
+        self.participants.append(participant)
 
-def bubble_sort(participants):
-    n = len(participants)
-    for i in range(n):
-        for j in range(n - 1 - i):
-            if participants[j].final_score < participants[j + 1].final_score:
-                participants[j], participants[j + 1] = participants[j + 1], participants[j]
+    def sort_participants(self):
+        for i in range(len(self.participants)):
+            for j in range(0, len(self.participants) - i - 1):
+                if self.participants[j].calculate_final_score() < self.participants[j + 1].calculate_final_score():
+                    # Меняем местами
+                    self.participants[j], self.participants[j + 1] = self.participants[j + 1], self.participants[j]
 
-participants = []
-n = int(input("Введите количество участников: "))
-for _ in range(n):
-    name = input("Введите фамилию участника: ")
-    scores = []
-    for i in range(5):
-        score = int(input(f"Введите оценку судьи {i + 1} (0-20): "))
-        scores.append(score)
-    jump_distance = float(input("Введите дальность прыжка (в метрах): "))
-    participant = Participant(name, scores, jump_distance)
-    participants.append(participant)
+    def display_results(self):
+        self.sort_participants()
+        print(f"{'Место':<6} {'Фамилия':<15} {'Итоговый результат':<20}")
+        for index, participant in enumerate(self.participants, start=1):
+            print(f"{index:<6} {participant.surname:<15} {participant.calculate_final_score():<20}")
 
-bubble_sort(participants)
+competition = Competition()
+competition.add_participant("Билявский", [18, 17, 19, 20, 16], 125)  
+competition.add_participant("Милованова", [15, 16, 14, 20, 15], 115)
+competition.add_participant("Федотова", [20, 19, 18, 20, 19], 130)
 
-print("\nИтоговая таблица соревнований:")
-print("{:<20} {:<10}".format("Фамилия", "Итоговый результат"))
-print("-" * 30)
-for participant in participants:
-    print("{:<20} {:<10}".format(participant.name, participant.final_score))
+competition.display_results()
 
 #ур3№1 Результаты сессии содержат оценки 5 экзаменов по каждой группе. Определить средний балл для трех групп студентов одного потока и выдать список групп в порядке убывания среднего балла. Результаты вывести в виде таблицы с заголовком.
 class Group:
