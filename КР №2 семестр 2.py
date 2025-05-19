e6e21cfdd697408ea537f617dcce1b066551e444
Ann-Milovanova <annamilovanova30@gmail.com>
2025-05-19 09:26:14 +0300

    Update КР №2 семестр 2.py

--- КР №2 семестр 2.py
+++ КР №2 семестр 2.py
@@ -1,85 +1,94 @@
+# Часть специалиста по данным
+
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from sklearn.datasets import make_blobs
+
+X, y = make_blobs(
+    n_samples=300, n_features=3, centers=3, cluster_std=1.2, random_state=42
+)
+
+data = pd.DataFrame(np.column_stack((X, y)), columns=["x", "y", "z", "class"])
+data.to_csv("3d_classes.csv", index=False)
+print("Данные сохранены в '3d_classes.csv'")
+
+
+# Часть разработчика
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
-from sklearn.inspection import DecisionBoundaryDisplay
 
-X, y = make_blobs(
-    n_samples=300,
-    n_features=3,
-    centers=3,
-    cluster_std=1.5,
-    center_box=(-10, 10),
-    random_state=42,
-)
-
-data = pd.DataFrame(
-    np.hstack([X, y.reshape(-1, 1)]), columns=["x1", "x2", "x3", "class"]
-)
-data.to_csv("3d_classes.csv", index=False)
+data = pd.read_csv("3d_classes.csv")
+X = data[["x", "y", "z"]].values
+y = data["class"].values
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=42
 )
 
 knn = KNeighborsClassifier()
-param_grid_knn = {"n_neighbors": [3, 5, 7, 9], "weights": ["uniform", "distance"]}
-grid_knn = GridSearchCV(knn, param_grid_knn, cv=5)
-grid_knn.fit(X_train, y_train)
-print(f"Лучшие параметры KNN: {grid_knn.best_params_}")
+knn_params = {"n_neighbors": [3, 5, 7], "weights": ["uniform", "distance"]}
+knn_grid = GridSearchCV(knn, knn_params, cv=5)
+knn_grid.fit(X_train, y_train)
 
 svm = SVC(kernel="linear")
-param_grid_svm = {"C": [0.1, 1, 10], "gamma": ["scale", "auto"]}
-grid_svm = GridSearchCV(svm, param_grid_svm, cv=5)
-grid_svm.fit(X_train, y_train)
-print(f"Лучшие параметры SVM: {grid_svm.best_params_}")
+svm_params = {"C": [0.1, 1, 10], "gamma": ["scale", "auto"]}
+svm_grid = GridSearchCV(svm, svm_params, cv=5)
+svm_grid.fit(X_train, y_train)
+
+print(f"Лучшие параметры KNN: {knn_grid.best_params_}")
+print(f"Лучшие параметры SVM: {svm_grid.best_params_}")
+
+# Часть дизайнера
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 fig = plt.figure(figsize=(18, 6))
 
 ax1 = fig.add_subplot(131, projection="3d")
-ax1.scatter(X[:, 0], X[:, 1], X[:, 2], c=y, cmap="viridis")
+ax1.scatter(X[:, 0], X[:, 1], X[:, 2], c=y, cmap="viridis", s=30)
 ax1.set_title("Исходные данные")
 
 ax2 = fig.add_subplot(132, projection="3d")
 ax2.scatter(
-    X_test[:, 0], X_test[:, 1], X_test[:, 2], c=grid_knn.predict(X_test), cmap="viridis"
+    X_test[:, 0],
+    X_test[:, 1],
+    X_test[:, 2],
+    c=knn_grid.predict(X_test),
+    cmap="viridis",
+    s=30,
 )
 ax2.set_title("KNN классификация")
 
 ax3 = fig.add_subplot(133, projection="3d")
 ax3.scatter(
-    X_test[:, 0], X_test[:, 1], X_test[:, 2], c=grid_svm.predict(X_test), cmap="viridis"
+    X_test[:, 0],
+    X_test[:, 1],
+    X_test[:, 2],
+    c=svm_grid.predict(X_test),
+    cmap="viridis",
+    s=30,
 )
 
-for i in range(len(grid_svm.best_estimator_.coef_)):
-    w = grid_svm.best_estimator_.coef_[i]
-    b = grid_svm.best_estimator_.intercept_[i]
+xlim = ax3.get_xlim()
+ylim = ax3.get_ylim()
 
-    xx, yy = np.meshgrid(np.linspace(-15, 15, 10), np.linspace(-15, 15, 10))
-    zz = (-w[0] * xx - w[1] * yy - b) / w[2]
-    ax3.plot_surface(xx, yy, zz, alpha=0.3, color="gray")
+xx = np.linspace(xlim[0], xlim[1], 30)
+yy = np.linspace(ylim[0], ylim[1], 30)
+XX, YY = np.meshgrid(xx, yy)
 
-ax3.set_title("SVM с разделяющими гиперплоскостями")
+for i in range(len(svm_grid.best_estimator_.coef_)):
 
+    w = svm_grid.best_estimator_.coef_[i]
+    b = svm_grid.best_estimator_.intercept_[i]
+
+    ZZ = (-w[0] * XX - w[1] * YY - b) / w[2]
+
+    ax3.plot_surface(
+        XX, YY, ZZ, alpha=0.3, color="gray", rstride=5, cstride=5, linewidth=0
+    )
+
+ax3.set_title("SVM с полными гиперплоскостями")
 plt.tight_layout()
-plt.show()
-
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
-
-disp_knn = DecisionBoundaryDisplay.from_estimator(
-    grid_knn.best_estimator_, X[:, [0, 1]], response_method="predict", alpha=0.5, ax=ax1
-)
-disp_knn.ax_.scatter(X[:, 0], X[:, 1], c=y, edgecolor="k")
-ax1.set_title("KNN (2D проекция)")
-
-disp_svm = DecisionBoundaryDisplay.from_estimator(
-    grid_svm.best_estimator_, X[:, [0, 1]], response_method="predict", alpha=0.5, ax=ax2
-)
-disp_svm.ax_.scatter(X[:, 0], X[:, 1], c=y, edgecolor="k")
-ax2.set_title("SVM (2D проекция)")
-
 plt.show()
